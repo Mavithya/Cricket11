d1fd93c010cfc8a382a1a858f408858a6c0fcf9f
Mavithya <mihirudmavithya@gmail.com>
2025-03-09 19:36:57 +0530

    u18

--- src/components/Cards/CardProfile.vue
+++ src/components/Cards/CardProfile.vue
@@ -1,39 +1,39 @@
 <template>
   <div
-    class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16"
+    class="relative flex flex-col w-full min-w-0 mt-16 mb-6 break-words bg-white rounded-lg shadow-xl"
   >
     <div class="px-6">
       <div class="flex flex-wrap justify-center">
-        <div class="w-full px-4 flex justify-center">
+        <div class="flex justify-center w-full px-4">
           <div class="relative">
             <img
               alt="..."
               :src="team2"
-              class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
+              class="absolute h-auto -m-16 -ml-20 align-middle border-none rounded-full shadow-xl lg:-ml-16 max-w-150-px"
             />
           </div>
         </div>
-        <div class="w-full px-4 text-center mt-20">
-          <div class="flex justify-center py-4 lg:pt-4 pt-8">
-            <div class="mr-4 p-3 text-center">
+        <div class="w-full px-4 mt-20 text-center">
+          <div class="flex justify-center py-4 pt-8 lg:pt-4">
+            <div class="p-3 mr-4 text-center">
               <span
-                class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
+                class="block text-xl font-bold tracking-wide uppercase text-blueGray-600"
               >
                 22
               </span>
               <span class="text-sm text-blueGray-400">Friends</span>
             </div>
-            <div class="mr-4 p-3 text-center">
+            <div class="p-3 mr-4 text-center">
               <span
-                class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
+                class="block text-xl font-bold tracking-wide uppercase text-blueGray-600"
               >
                 10
               </span>
               <span class="text-sm text-blueGray-400">Photos</span>
             </div>
-            <div class="lg:mr-4 p-3 text-center">
+            <div class="p-3 text-center lg:mr-4">
               <span
-                class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
+                class="block text-xl font-bold tracking-wide uppercase text-blueGray-600"
               >
                 89
               </span>
@@ -42,35 +42,32 @@
           </div>
         </div>
       </div>
-      <div class="text-center mt-12">
+      <div class="mt-12 text-center">
         <h3
-          class="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2"
+          class="mb-2 text-xl font-semibold leading-normal text-blueGray-700"
         >
-          Jenna Stones
+        Angelo Jayawardene
         </h3>
         <div
-          class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase"
+          class="mt-0 mb-2 text-sm font-bold leading-normal uppercase text-blueGray-400"
         >
-          <i class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
-          Los Angeles, California
+          <i class="mr-2 text-lg fas fa-map-marker-alt text-blueGray-400"></i>
+          Galle , Sri Lanka
         </div>
-        <div class="mb-2 text-blueGray-600 mt-10">
-          <i class="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
-          Solution Manager - Creative Tim Officer
+        <div class="mt-10 mb-2 text-blueGray-600">
+          <i class="mr-2 text-lg fas fa-briefcase text-blueGray-400"></i>
+          Left Handed Batsman
         </div>
         <div class="mb-2 text-blueGray-600">
-          <i class="fas fa-university mr-2 text-lg text-blueGray-400"></i>
-          University of Computer Science
+          <i class="mr-2 text-lg fas fa-university text-blueGray-400"></i>
+          University of Colombo
         </div>
       </div>
-      <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
+      <div class="py-10 mt-10 text-center border-t border-blueGray-200">
         <div class="flex flex-wrap justify-center">
-          <div class="w-full lg:w-9/12 px-4">
+          <div class="w-full px-4 lg:w-9/12">
             <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
-              An artist of considerable range, Jenna the name taken by
-              Melbourne-raised, Brooklyn-based Nick Murphy writes, performs and
-              records all of his own music, giving it a warm, intimate feel with
-              a solid groove structure. An artist of considerable range.
+              
             </p>
             <a href="javascript:void(0);" class="font-normal text-emerald-500">
               Show more
