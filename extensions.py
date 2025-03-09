70afe8c4fbd4d083376b79bdad71e02c5531e72d
Mahagamage H.D. <121503609+HarinDulneth@users.noreply.github.com>
2025-03-09 17:58:43 +0530

    Delete extensions.py

--- extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
-from flask_socketio import SocketIO
-from flask_pymongo import PyMongo
-
-bcrypt = Bcrypt()
-jwt = JWTManager()
-socketio = SocketIO()
-mongo = PyMongo()
