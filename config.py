fc2989a104eacceda320ed82b9d18b7e8b820cb7
Mahagamage H.D. <121503609+HarinDulneth@users.noreply.github.com>
2025-03-09 17:58:32 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from datetime import timedelta
-
-class Config:
-    MONGO_URI = "mongodb://localhost:27017/fantasy_cricket"
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET", "your-secret-key")
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
